4e0ad44b8cfbd5d3cf1a30d07e25ae6c2cee9c2e
Semyon Komyagin <komyagin.s@gmail.com>
2020-05-23 23:55:47 +0300

    Initial commit

---
+++ src/main/java/setup/IPageObject.java
@@ -0,0 +1,9 @@
+package setup;
+
+import org.openqa.selenium.WebElement;
+
+public interface IPageObject {
+
+    WebElement getWebElement(String weName) throws NoSuchFieldException, IllegalAccessException, InstantiationException;
+
+}
